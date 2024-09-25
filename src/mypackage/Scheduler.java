e27f214eb36018f12674a274b63505ce0da3c176
croaa <croaa1ruizr@gmail.com>
2024-09-25 12:10:10 -0400

    Some work done to Scheduler.java

--- src/mypackage/Scheduler.java
+++ src/mypackage/Scheduler.java
@@ -1,9 +1,33 @@
 package mypackage;
+
+import java.util.Scanner;
 /**
  * @author Christian Roa
  */
 public class Scheduler {
-    public void run(){
+    private Scanner scanner; // reads user input
 
+    public Scheduler() {
+        scanner = new Scanner(System.in);
+    }
+
+    public void run(){
+        System.out.println("Scheduler is running.");
+        String command;
+
+        while(true) {
+            System.out.print("> ");
+            command = scanner.nextLine().trim();
+
+            if(command.isEmpty()) {
+                continue;
+            }
+
+            if(command.equals("Q")) {
+                System.out.println("Scheduler terminated");
+                break;
+            }
+        }
+        scanner.close();
     }
 }
