17831cdd2d6bc43248afc37ec68ee61754f63c00
Renil Khristi <khristirenil@gmail.com>
2024-09-28 15:59:30 -0400

    Update MedicalRecord.java
    Implementing the Medical Records class

--- src/mypackage/MedicalRecord.java
+++ src/mypackage/MedicalRecord.java
@@ -1,8 +1,39 @@
 package mypackage;
 /**
- * @author Christian Roa
+ * @author Renil Khristi
  */
 public class MedicalRecord {
+	
     private Patient[] patients;
     private int size;
+    
+    public MedicalRecord() {
+    	this.patients = new Patient[0];
+    	this.size = 0;    	
+    }
+    
+    public MedicalRecord(int size) {
+    	this.patients = new Patient[size];
+    	this.size = size;
+    }
+    
+    public boolean add(Patient patient) {
+    	if(size < patients.length) {
+    		patients[size] = patient;
+    		size++;
+    		return true;
+    	}
+    	return false;
+    }
+    
+    public int getSize() {
+    	return size;
+    }
+    
+    public Patient getPatient(int i) {
+    	if(i >= 0 && i < size) { 
+    		return patients[i];
+    	}
+    	return null;
+    }
 }
