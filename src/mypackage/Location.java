f3fbcf150b6d218420443ac487e0920698849729
Renil Khristi <khristirenil@gmail.com>
2024-09-28 15:54:03 -0400

    Update Location.java
    Implemented the Location class

--- src/mypackage/Location.java
+++ src/mypackage/Location.java
@@ -1,9 +1,23 @@
 package mypackage;
 /**
- * @author Christian Roa
+ * @author Renil Khristi
  */
 public enum Location {
+    BRIDGEWATER("Somerset County", "08807"), EDISON("Middlesex County", "08817"), PISCATAWAY("Middlesex County", "08854"), PRINCETON("Mercer County", "08542"), MORRISTOWN("Morris County", "07960"), CLARK("Union County", "07066");
+
     private final String county;
     private final String zip;
 
+    Location(String county, String zip){
+        this.county = county;
+        this.zip = zip;
+    }
+    
+    public String getCounty() {
+    	return county;
+    }
+    
+    public String getZip() {
+    	return zip;
+    }    
 }
