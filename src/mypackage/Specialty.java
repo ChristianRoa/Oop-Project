460691f128f8b21afbec6c7dae6bd049308876c4
Renil Khristi <khristirenil@gmail.com>
2024-09-30 21:35:40 -0400

    Update Specialty.java
    Added comments

--- src/mypackage/Specialty.java
+++ src/mypackage/Specialty.java
@@ -1,15 +1,24 @@
 package mypackage;
 /**
+An enum class describing the specialty of the providers and the charge that they demand
  * @author Renil Khristi, Christian Roa
  */
 public enum Specialty {
     FAMILY(250), PEDIATRICIAN(300), ALLERGIST(350);
     private final int charge;
 
+    /*
+    Gets the county 
+    @param charge the amount the provider charges
+     */
     Specialty(int charge) {
         this.charge = charge;
     }
-    
+
+    /*
+    Gets the charge
+    @return the charge
+     */
     public int getCharge() {
     	return charge;
     }
